1057f73fe25965f52d0caed586650dd9cb50cbd2
Gabriel Leal <gabrielthleal@gmail.com>
2020-08-21 15:23:37 -0300

    bot shows the song name based on youtube yrl

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'webhooks/telegram_vbc43edbf1614a075954dvd4bfab34l12', to: 'webhooks#callback'
+  post 'webhooks/telegram_vbc43edbf1614a075954dvd4bfab34l12', to: 'webhooks#callback'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- lib/telegram_bot/command/search.rb
+++ lib/telegram_bot/command/search.rb
@@ -12,10 +12,12 @@ module TelegramBot
       end
 
       def start
-        send_message("The tile of that song is: #{get_title}")
+        send_message('Wait a minute...')
+        send_message("The tile of that song is: \n #{get_title}")
 
         user.reset_next_bot_command
         
+        user.next_bot_command = 'TelegramBot::Command::Start'
         # search spotify track here
       end
 
@@ -26,18 +28,22 @@ module TelegramBot
         uri = URI("https://www.googleapis.com/youtube/v3/videos?id=#{yt_video_id}&key=#{yt_api_key}&part=snippet")
 
         res = JSON.parse(Net::HTTP.get(uri))
+        
+        result = res['items'][0]['snippet']['title']
 
-        res['items'][0]['snippet']['title']
+        return 'Not found' if result.nil?
+
+        result
       end
 
       def video_id
         match = text.match(
           %r{^.*((youtu.be/)|(v/)|(/u/\w/)|(embed/)|(watch\?))\??v?=?([^#\&\?]*).*}
         )
+        
+        return match[7] if match.size == 8
 
-        match[7] if match.size == 8
-
-        'bakamitai kazuma kiryu'
+        nil
       end
     end
   end
