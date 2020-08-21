1b3e9f1111b6a92a2c3ddf0bd66d04c3d2e40d9d
Gabriel Leal <gabrielthleal@gmail.com>
2020-08-21 15:23:01 -0300

    add ngrok to whitelist and add lib to eager_load_paths

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module SongCatcherBot
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.eager_load_paths << "#{Rails.root}/lib"
   end
 end
