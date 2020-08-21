b6c3a3a7ea7ef4690596f5a8a21106eeb04d4871
Gabriel Leal <gabrielthleal@gmail.com>
2020-08-21 14:24:22 -0300

    add start command

---
+++ lib/telegram_bot/command/start.rb
@@ -0,0 +1,19 @@
+# frozen_String_literal: true
+
+module TelegramBot
+  module Command
+    class Start < Base
+      def should_start?
+        text.match?(%r{\A/start})
+      end
+
+      def start
+        send_message('Welcome. Send me a youtube url')
+
+        user.reset_next_bot_command
+
+        user.next_bot_command = 'TelegramBot::Command::Search'
+      end
+    end
+  end
+end
