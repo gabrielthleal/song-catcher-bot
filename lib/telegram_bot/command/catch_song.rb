57679e2c3193399a3abd35b8e1eee96a4bb5febf
Gabriel Leal <gabrielthleal@gmail.com>
2020-09-05 15:32:26 -0300

    fix start again

--- lib/telegram_bot/command/catch_song.rb
+++ lib/telegram_bot/command/catch_song.rb
@@ -10,30 +10,52 @@ module TelegramBot
       end
 
       def start
-        return send_message(I18n.t('telegram.authorized')) if can_search?
+        return user.next_bot_command = 'TelegramBot::Command::Start' if start_again?
 
-        return send_message(I18n.t('telegram.authorization'), { with_markup: true, inline_keyboard: { text: I18n.t('telegram.authorization_btn'), url: autorization_link } }) if spotify_user.nil?
+        return send_message(I18n.t('telegram.authorized')) if should_search?
+
+        return send_message(I18n.t('telegram.authorization'), authorization) if spotify_user.nil?
 
         send_message(I18n.t('telegram.searching'))
 
         return send_message(I18n.t('telegram.not_found')) if track.nil?
 
-        send_message("#{I18n.t('telegram.add_song')}#{track['external_urls']['spotify']}", { with_markup: true, inline_keyboard: { text: I18n.t('telegram.add_song_btn'), callback_data: track['uri'] } })
+        send_message("#{I18n.t('telegram.add_song')}#{track['external_urls']['spotify']}", add_song_btn)
       end
 
-      def autorization_link
-        SpotifyApi::Authorization.generate_link(user.id)
+      def authorization
+        {
+          with_markup: true, inline_keyboard:
+          {
+            text: I18n.t('telegram.authorization_btn'),
+            url: SpotifyApi::Authorization.generate_link(user.id)
+          }
+        }
       end
 
-      def can_search?
+      def add_song_btn
+        {
+          with_markup: true, inline_keyboard:
+          {
+            text: I18n.t('telegram.add_song_btn'),
+            callback_data: track['uri']
+          }
+        }
+      end
+
+      def should_search?
         secret_code = Base64.urlsafe_encode64('potato123')
 
         text.include?(secret_code)
       end
 
+      def start_again?
+        text.match?(%r{\A/start})
+      end
+
       def track
         @track ||= SpotifyApi::Search.track(text, spotify_user)
-        
+
         @track['tracks']['items'][0]
       end
 
