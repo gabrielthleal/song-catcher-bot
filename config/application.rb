a47a42470caccfb0aa026d04f7423407e89fcb5d
Gabriel Leal <gabrielthleal@gmail.com>
2020-09-09 20:57:42 -0300

    add new messges and create language field to user table

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module SongCatcherBot
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
 
-    config.i18n.default_locale = 'pt-br'
+    config.i18n.default_locale = 'en'
 
     config.api_only = true
     config.eager_load_paths << "#{Rails.root}/lib"
